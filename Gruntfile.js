118f8e051bdefdc880b41bd2dc3c46389b35f00e
Yamishi Yuuki Nekozawa <yamishi.atarashianime@gmail.com>
2014-03-18 08:32:13 -0600

    setting testing on travis

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        lint: {
+            files: ['*.js']
+        },
+        min: {
+            files: ['*.js']
+        }
+    });
+
+    grunt.registerTask('travis', 'lint min';
+};
--- checky.js
+++ checky.js
@@ -1,1 +1,16 @@
 //checky.js
+function checky() {
+    this.checks = {"ch": []};
+    if(localStorage.checks === null) {
+        localStorage.checks = JSON.stringify(this.checks);
+    }
+
+    this.checks = JSON.parse(localStorage.checks);
+    this.checks.ch.forEach(function(c) {
+        //var curr = document.getElementById(c.nombre);
+    });
+
+    function onUpdate (id) {
+        document.getElementById(id);
+    }
+}
